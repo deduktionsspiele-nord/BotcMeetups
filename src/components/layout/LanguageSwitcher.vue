8f06a6d70e2563dcc45b2c93f1f90f07e25e42f9
Toasting Machine <Toastgeraet@users.noreply.github.com>
2025-05-15 22:43:33 +0200

    Updated LanguageSwitcher.vue

--- src/components/layout/LanguageSwitcher.vue
+++ src/components/layout/LanguageSwitcher.vue
@@ -6,7 +6,7 @@
       aria-haspopup="true"
       :aria-expanded="isOpen"
     >
-      <span class="text-sm">{{ currentLanguage }}</span>
+      <div class="inline-block p-2 bg-color-white"><span class="text-sm">{{ currentLanguage }}</span>
       <svg 
         xmlns="http://www.w3.org/2000/svg" 
         class="h-4 w-4" 
