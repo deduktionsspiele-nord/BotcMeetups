6050fba4c69a6742bd55bfb5e5494b854a15257f
toastgeraet <bergmann.tobias@posteo.de>
2025-05-16 00:53:10 +0200

    fix import of logo asset

--- src/components/ui/Logo.vue
+++ src/components/ui/Logo.vue
@@ -5,7 +5,7 @@
     :rel="external ? 'noopener noreferrer' : undefined"
     class="flex items-center space-x-2"
   >
-    <div class="w-10 h-10 bg-contain bg-center bg-no-repeat" style="background-image: url('/src/assets/images/DSN_Logo_screen_new.png')"></div>
+    <div class="w-10 h-10 bg-contain bg-center bg-no-repeat" :style="{ backgroundImage: `url(${logoUrl})` }"></div>
     <span class="font-display text-xl font-bold text-floral-500 ">
       {{ $t('organisation.longName') }}
     </span>
@@ -16,6 +16,8 @@
 </template>
 
 <script setup lang="ts">
+import logoUrl from '@/assets/images/DSN_Logo_screen_new.png'
+
 defineProps({
   href: {
     type: String,
